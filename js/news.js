e7dd565e021ddb32e6944637e1b3bfbc553f448e
Joti <tanjilajoti@gmail.com>
2022-09-03 01:56:21 +0600

    show news number

--- js/news.js
+++ js/news.js
@@ -1,3 +1,4 @@
+
 const loadCategory = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
@@ -5,20 +6,48 @@ const loadCategory = () => {
 }
 const displayCategory = cat => {
     const blockCategory = document.getElementById('category');
-    console.log(cat);
+
     const category = cat.data.news_category;//.category_name;
     for (const catElement of category) {
+        // console.log(catElement.category_name);
+        //name = catElement.category_name;
         const catDiv = document.createElement('div');
         catDiv.classList.add('mx-5');
         catDiv.classList.add('text-secondary');
         catDiv.innerHTML = `
 
-        <p> ${catElement.category_name}</p>
+        <p onclick="NewsNumLoad('${catElement.category_id}')"> ${catElement.category_name}</p>
     
         `
         blockCategory.appendChild(catDiv);
-        // console.log(catElement.category_name);
+        // console.log(catElement.category_id);
+        //onclick="loadCountryDetail('${catElement.category_id}')"
     }
-    // console.log(cat);//.data.news_category[0].category_name)
+    //  console.log(cat.data.news_category[0].category_id)
 }
 loadCategory();
+
+
+const NewsNumLoad = (code) => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${code}`)
+        .then(res => res.json())
+        .then(data => displayNewsNum(data))
+}
+const displayNewsNum = (newsNo) => {
+
+    const blockNewsNo = document.getElementById('newsNo');
+    //console.log(name);
+    blockNewsNo.innerHTML = '';
+    const newsNoDiv = document.createElement('div');
+
+    newsNoDiv.classList.add('p-2');
+    newsNoDiv.classList.add('text-center');
+    console.log(blockNewsNo.childNodes)
+    newsNoDiv.innerHTML = `
+    <p>${newsNo.data.length} found for this Category </p>
+    `
+    blockNewsNo.appendChild(newsNoDiv);
+
+
+}
+//NewsNumLoad();
